1459daa0d894e0ac5343223c1491a81ac30ed9f6
smrsan <57676740+smrsan@users.noreply.github.com>
2025-02-01 23:30:24 +0330

    Remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { CssBaseline } from "@mui/material";
 
 import Login from "./Login.tsx";
 
-import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
