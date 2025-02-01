02e26d3154223aff7238fc45845a20e1ab4696c9
smrsan <57676740+smrsan@users.noreply.github.com>
2025-02-01 23:29:58 +0330

    Call reqres API endpoints in Login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,45 @@
+import { ChangeEventHandler, useCallback, useState } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 function Login() {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorText, setErrorText] = useState("");
+
+  const handleChange = useCallback<
+    ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
+  >((event) => {
+    setFormValues((p) => ({
+      ...p,
+      [event.target.name]: event.target.value,
+    }));
+  }, []);
+
+  const handleLogin = useCallback(async () => {
+    const res = await fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formValues),
+    });
+
+    const data = await res.json();
+
+    if (data.error) {
+      setErrorText(data.error);
+      return;
+    }
+
+    console.log(data);
+  }, [formValues]);
+
   return (
     <Box
       sx={{
@@ -24,9 +60,35 @@ function Login() {
           gap: 2,
         }}
       >
-        <TextField label="Username" fullWidth autoFocus />
-        <TextField label="Password" fullWidth type="password" />
-        <Button>Login</Button>
+        <TextField
+          name="email"
+          onChange={handleChange}
+          value={formValues["email"]}
+          label="Email"
+          fullWidth
+          autoFocus
+          type="email"
+        />
+        <TextField
+          name="password"
+          onChange={handleChange}
+          value={formValues["password"]}
+          label="Password"
+          fullWidth
+          type="password"
+        />
+        {errorText && (
+          <Typography
+            variant="subtitle2"
+            sx={{
+              textTransform: "capitalize",
+              color: (theme) => theme.palette.error.main,
+            }}
+          >
+            {errorText}
+          </Typography>
+        )}
+        <Button onClick={handleLogin}>Login</Button>
       </Paper>
     </Box>
   );
