ee189d14e1dbeb19638661f99db9dfe3c7fd2454
smrsan <57676740+smrsan@users.noreply.github.com>
2025-02-01 22:45:58 +0330

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+
+import App from "./App.tsx";
+
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <CssBaseline />
+    <App />
+  </StrictMode>
+);
+
